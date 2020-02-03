163db9d7c163e4f6001ca016fa3e1f17893aa1b2
Tim Kettenacker <timkettenacker@Tims-MacBook-Pro.local>
2020-02-03 21:04:30 +0100

    extended code with some routing and some cocoon functionality

--- index.py
+++ index.py
@@ -2,15 +2,30 @@
 # pip install python-dotenv
 # pip install -r requirements.txt
 # flask run
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from flask_bootstrap import Bootstrap
 
+
+from textblob import TextBlob, Word
+import random
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/analyse', methods=['POST'])
+def analyse():
+    if request.method == 'POST':
+        rawtext = request.form['rawtext']
+        blob = TextBlob(rawtext)
+        blob_sentiment, blob_subjectivity = blob.sentiment.polarity, blob.subjectivity
+        tokens = [blob.words]
+
+    return render_template('index.html', received_text = blob, tokens=tokens, blob_sentiment = blob_sentiment,
+                           blob_subjectivity = blob_subjectivity)
 
 if __name__ == '__main__':
     app.run(debug=True)
