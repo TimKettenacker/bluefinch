ea2ea9d1881fe2334bf4602914223c834b116e3b
Tim Kettenacker <timkettenacker@Tims-MacBook-Pro.local>
2020-03-22 19:22:43 +0100

    added some logic based on prediction criteria (in writing only so far)

--- nlu.py
+++ nlu.py
@@ -8,7 +8,6 @@ import de_core_news_sm
 from collections import defaultdict
 
 # this section needs to be triggered before the user can converse with the chatbot
-# model = fasttext.train_supervised(input="training_set_intents", lr=0.9, epoch = 25, wordNgrams=2)
 model = fasttext.load_model("ml_model/model_intent_detection.bin")
 nlp = de_core_news_sm.load()
 onto_path.append("ontology_material")
@@ -22,11 +21,7 @@ for individual in onto.individuals():
     individuals[individual.name] = [individual.iri, individual.is_a.first(), individual.is_instance_of.first()]
 # second, a detected matching iri goes into ontology to retrieve node connections
 
-userText = "welche iphones hast du?" # welche iphones gibt es? habt ihr iphone 11?
-# wie groß ist der speicher? wie viel speicher hat es?
-# was ist der vorteil von pro? warum ist pro besser?
-# was kostet das? wie teuer ist das? was ist der preis?
-# kann ich mit mastercard bezahlen?
+userText = "welche iphones hast du?"
 
 # capitalize every first letter per word to increase noun detection
 doc = nlp(userText.title())
@@ -34,7 +29,16 @@ doc = nlp(userText.title())
 # further may extend to look for predicates
 # fuzzy search keys in individuals for possible hits, i.e. 'AmericanExpress' in individuals.keys()
 # pass lowercase to model prediction, because training data is also lowercase so prediction is allergic to upper case
-model.predict(doc.text.lower())
+prediction = model.predict(doc.text.lower())
+if (prediction[1].item() > .7) == True:
+    print(prediction[0]) # if the predicted label meets the criteria, classify sentence type is called
+    # to see whether the result from the model is off; if it is off, jump to "else", if it is not
+    # trigger the lookup in the ontology to find the matching noun and choose suiting response
+    # from response generation module
+
+else:
+    # return default response; write input + output to log
+    print("Entschuldigung, das habe ich nicht ganz verstanden")
 
 
 # first, classify what the user wants to do, even if the product is already mentioned. Classify into
@@ -42,9 +46,10 @@ model.predict(doc.text.lower())
 # if there is a clear match between entity of interest and ontology after classifying the intent,
 # it is easier to reply back
 # entity_of_interest - in ontology > narrow down question
-# first, train on labelled data, but pass along rules of what it could have been using spacys
+
 
 def classify_sentence_type(doc):
+    # classifies sentence types according to shallow parsing results
     sentence_type = ''
     doc_json = doc.to_json()
     tags = []
