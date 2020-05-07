8cb9131d5ce9b54ae74cf1f50b5eb70f1abde802
Tim <timkettenacker@Tims-MacBook-Pro.local>
2020-05-07 18:56:57 +0200

    changed function name inside chatbot to reflect what it actually does

--- chatbot.py
+++ chatbot.py
@@ -25,19 +25,20 @@ class Chatbot(object):
         return "This is an instance of class chatbot with a unique identifier {} " \
                "that was created on {}".format(self.uuid, self.created_at)
 
-    def update_conversation(self, input=None, **kwargs):
+    def grasp_intent(self, input=None, **kwargs):
         """
-        Updates the ongoing conversation with a user. First, it increases the count of conversational
-        exchanges by 1, then it passes the user input on to the natural language understanding module.
-        The natural language understanding module predicts the user intent utilizing the trainer module
+        This function aims at grasping the intent the user implies or conveys in his messages. It applies
+        various methods to get closer to the user's intentions: first, it passes the input to the natural
+        language understanding module, which in turn predicts the user intent utilizing the trainer module
         and validates the plausibility of the trainer's predicted intent.
 
         If the intermediate results fall into the confidence interval, they are then cross-checked with
-        the ontology search and reasoning module which compares the findings to the current and historical
-        context.
+        the ontology search and reasoning module which tries to establish a match between the recognized
+        entities and the classes and individuals in the ontology.
 
-        Any findings are then passed back to this calling function to steer the conversation. If one of
-        the functions called returns an empty result, default_response() is invoked.
+        Any findings are then passed back to this calling function to update the context node and further
+        steer the conversation. If one of the functions called returns an empty result, default_response()
+        is invoked.
 
         :param input: input of the user (in response to the latest context)
         :param kwargs:
