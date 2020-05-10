2603b5bb8b813a7fe3c16bfc06affecc6c8e4a64
Tim <timkettenacker@Tims-MacBook-Pro.local>
2020-05-10 12:19:43 +0200

    changed files so now on startup of index.py, my code will be used instead of chatterbot

--- conversation_context.py
+++ conversation_context.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from collections import defaultdict
 import csv
 import random
-import chatbot
 
 class ConversationContext(object):
 
--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# in cmd: navigate to project directory
 # source venv/bin/activate
 # pip install python-dotenv
 # pip install -r requirements.txt
@@ -5,44 +7,21 @@
 # flask run
 from flask import Flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
-from CustomTrainer import CustomTrainer
-
+from chatbot import Chatbot
 
 app = Flask(__name__)
 Bootstrap(app)
 
-bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter",
-                logic_adapters=[
-                        {
-                            'import_path': 'chatterbot.logic.BestMatch',
-                            'default_response': 'Entschuldigung, das habe ich nicht ganz verstanden.',
-                            'maximum_similarity_threshold': 0.90
-                        }
-                    ]
-              )
-trainer = ChatterBotCorpusTrainer(bot)
-trainer.train("chatterbot.corpus.german.conversations", "chatterbot.corpus.german.greetings")
-#trainer = CustomTrainer(bot)
-#trainer.train("chatterbot.corpus.german.conversations", "chatterbot.corpus.german.greetings")
+bot = Chatbot()
 
 @app.route('/')
 def index():
-    # for future implementation: on startup, load the ontology in the background
     return render_template('index.html')
-
 
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    # parse input from the user (Natural Language Understanding)
-    # https://explosion.ai/blog/german-model
-    # https://github.com/adbar/German-NLP#Text-corpora
-    return str(bot.get_response(userText))
-
+    return str(bot.respond_user(input=userText))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
